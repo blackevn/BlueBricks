f4fb3e64e0f479467239a97451184e887a189560
blackevn <kevinlouie55@gmail.com>
2023-06-16 18:58:39 +0000

    Add property

--- app/components/Forms/countrySelect.tsx
+++ app/components/Forms/countrySelect.tsx
@@ -45,12 +45,12 @@ const CountrySelect: React.FC<CountrySelectProps> = ({
         )}
         className='select'
         classNames={{
-          option: () => 'bg-[#161616] ',
-          container: () => 'bg-[#161616]',
-          control: () => 'bg-[#161616]',
-          group: () => 'bg-[#161616]',
-          menu: () => 'bg-[#161616]',
-          menuList: () => 'bg-[#161616]',
+          option: () => 'bg-[#161616] border-none',
+          container: () => 'bg-[#161616] border-none',
+          control: () => 'bg-[#161616] border-none',
+          group: () => 'bg-[#161616] border-none',
+          menu: () => 'bg-[#161616] border-none',
+          menuList: () => 'bg-[#161616] border-none',
         }}
         theme={(theme) => ({
           ...theme,
