5dddb255170902991f28fdc5b1a193b8cf46ddc7
blackevn <kevinlouie55@gmail.com>
2023-06-17 10:53:29 +0000

    Add property

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -29,7 +29,7 @@ const Map: React.FC<MapProps> = ({ center }) => {
         center={center as L.LatLngExpression || [51, -0.09]} 
         zoom={center ? 4 : 2} 
         scrollWheelZoom={false} 
-        className="h-[30vh] w-[40vh] rounded-lg "
+        className="h-[30vh] w-[40vw] rounded-lg "
       >
         <TileLayer
           url={url}
