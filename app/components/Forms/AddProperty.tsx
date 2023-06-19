c336c468f8ec47b151390a541dccc5274a4c69b9
blackevn <kevinlouie55@gmail.com>
2023-06-19 09:04:28 +0000

    Add property - Map fix

--- app/components/Forms/AddProperty.tsx
+++ app/components/Forms/AddProperty.tsx
@@ -102,31 +102,31 @@ const AddProperty: React.FC<AddPropertyProps> = () => {
 
     const Map = useMemo(() => dynamic(() => import('../Map'), { 
       ssr: false 
-    }), [propertyInfo.location]);
+    }), [propertyInfo.location, step]);
 
 
   if (step === STEPS.LOCATION){
      
-    heading = (
-      <Heading 
-      title="Location"
-      label="Add your property"/>
-    )
-    bodyContent = (
-                  <div>
+    // heading = (
+    //   <Heading 
+    //   title="Location"
+    //   label="Add your property"/>
+    // )
+    // bodyContent = (
+    //               <div>
 
-                      <div className="place-items-center grid gap-4 relative h-[40vh]">
-                         <div className="z-[99] w-full absolute top-0 grid place-items-center">
-                         <CountrySelect 
-                         value={propertyInfo.location} 
-                         onChange={(value: any) => setPropertyInfo({...propertyInfo, location: value})} 
-                         />
-                         </div>
-                         <Map center={propertyInfo.location?.latlng}/>
-                       </div>
-                    </div>
+    //                   <div className="place-items-center grid gap-4 relative h-[40vh]">
+    //                      <div className="z-[99] w-full absolute top-0 grid place-items-center">
+    //                      <CountrySelect 
+    //                      value={propertyInfo.location} 
+    //                      onChange={(value: any) => setPropertyInfo({...propertyInfo, location: value})} 
+    //                      />
+    //                      </div>
+    //                      <Map center={propertyInfo.location?.latlng}/>
+    //                    </div>
+    //                 </div>
                     
-    )
+    // )
   }
 
 
