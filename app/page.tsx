8c06ce1b532a052d1a113560585a6d12e54b1a4d
blackevn <kevinlouie55@gmail.com>
2023-06-29 08:54:41 +0000

    Add property

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,16 @@ const page = async () => {
 
   let isLoading = false
 
-  return <div className='p-4'>
-          { !isLoading ? <ListLoading/> : <ListCard listings={listings}/>}
+  let loading: any = [ 0 , 1, 2, 3, 4, 5, 6 , 7, 8 ]
+ 
+  return <div className='p-8'>
+          { !isLoading ? 
+          <div>
+           { loading.map(() => <ListLoading/>) } 
+
+          </div>
+           : 
+          <ListCard listings={listings}/>}
           </div>;
 };
 
