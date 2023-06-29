8db7b0a1b8a8dd7180c4009b9b89b662d2a558b9
blackevn <kevinlouie55@gmail.com>
2023-06-29 07:48:29 +0000

    Add property

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { getListings } from "./actions";
 import { ListCard } from "./components";
 import { ListLoading } from "./components/Loading";
@@ -7,7 +6,7 @@ const page = async () => {
 
   const listings = await getListings()
 
-  const [ isLoading, setIsLoading ] = useState(false)
+  let isLoading = false
 
   return <div>
           { !isLoading ? <ListLoading/> : <ListCard listings={listings}/>}
