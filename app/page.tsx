123b61237453299d0ce38756ae02ff73ec9313d6
blackevn <kevinlouie55@gmail.com>
2023-06-30 16:11:41 +0000

    Add property

--- app/page.tsx
+++ app/page.tsx
@@ -12,15 +12,15 @@ const page = async () => {
 
   let loading: number[] = [ 0 , 1, 2, 3, 4, 5, 6 , 7, 8 ]
  
-  return <div className='p-8'>
+  return <div className='p-32'>
           { !listings ? 
-           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 place-items-center">
+           <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8 place-items-center">
            { loading.map(() => <ListLoading/>) } 
           </div>
 
            : 
           
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 place-items-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-8 place-items-center">
             {listings.map((list) => ( <ListCard 
                                        key={list.id} 
                                        listing={list}
