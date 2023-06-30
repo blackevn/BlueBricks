c8e2a5e2d42db6b981962a90e257b6ccc927d7c8
blackevn <kevinlouie55@gmail.com>
2023-06-30 16:23:01 +0000

    Add property

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { Listing } from "@/types/interfaces";
 import { getListings } from "./actions";
 import { ListCard } from "./components";
 import { ListLoading } from "./components/Loading";
@@ -12,9 +11,9 @@ const page = async () => {
 
   let loading: number[] = [ 0, 1, 2, 3, 4, 5, 6 , 7, 8 ]
  
-  return <div className='md:p-10 lg:p-32'>
+  return <div className='md:px-10 lg:px-32'>
           { !listings ? 
-           <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8 place-items-center">
+           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 place-items-center">
            { loading.map(() => <ListLoading/>) } 
           </div>
 
