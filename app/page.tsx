deca8b8da289f1c77b47cbd0ae54f9e8bbdb300d
blackevn <kevinlouie55@gmail.com>
2023-07-01 14:45:32 +0000

    Add property

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
+import { Listing } from "@/types/interfaces";
 import { getListings } from "./actions";
 import { ListCard } from "./components";
 import { ListLoading } from "./components/Loading";
-
+import { useEffect, useState } from "react";
+ 
 
 const page = async () => {
 
   const listings = await getListings()
-
-  let isLoading = false
-
+ 
   let loading: number[] = [ 0, 1, 2, 3, 4, 5, 6 , 7, 8 ]
  
   return <div className='px-5 md:px-10 lg:px-32'>
-          { !listings ? 
+          { !listings.find((list) => list.imageSrc) ?
            <div className="listGrid">
            { loading.map(() => <ListLoading/>) } 
           </div>
