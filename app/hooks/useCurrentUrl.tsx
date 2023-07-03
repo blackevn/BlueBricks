2c0ddc3fbea37a777c4ae9501c33514fc24fa41a
blackevn <kevinlouie55@gmail.com>
2023-07-03 12:18:59 +0000

    Add property

---
+++ app/hooks/useCurrentUrl.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+
+const useCurrentUrl = () => {
+  const [currentUrl, setCurrentUrl] = useState('');
+
+  useEffect(() => {
+    setCurrentUrl(window.location.href);
+  }, []);
+
+  return currentUrl;
+};
+
+export default useCurrentUrl;
--- app/listings/[istingId]/page.tsx
+++ app/listings/[istingId]/page.tsx
@@ -1,17 +1,10 @@
 
 import { getCurrentUser, getListingById, getReservations } from "@/app/actions";
+import useCurrentUrl from "@/app/hooks/useCurrentUrl";
 
 const ListingPage = async () => {
-// Get the query string portion of the URL
-const queryString = window?.location.search;
 
-// Create a new URLSearchParams object with the query string
-const urlParams = new URLSearchParams(queryString);
-
-// Retrieve the value of a specific parameter
-const param1Value = urlParams.get('param1'); // "value1"
-
-console.log(param1Value);
+  const currentUrl = useCurrentUrl();
 
   return (
     <></>
