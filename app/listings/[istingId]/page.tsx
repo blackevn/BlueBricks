3f591a33b10b5621dde05c7247acfcf1bb868442
blackevn <kevinlouie55@gmail.com>
2023-07-03 13:33:49 +0000

    Add property

--- app/listings/[istingId]/page.tsx
+++ app/listings/[istingId]/page.tsx
@@ -1,5 +1,6 @@
 
 import { getCurrentUser, getListingById, getReservations } from "@/app/actions";
+import { GetServerSideProps } from "next";
 
 
 interface IParams {
@@ -10,10 +11,23 @@ const ListingPage = async ({ params }: {params: IParams}) => {
 
 const listingId = await getListingById(params)
   
+console.log(params);
+
 
   return (
     <></>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { params } = context;
+
+  return {
+    props: {
+      params: params || ''
+    }
+  };
+};
+
  
 export default ListingPage;
