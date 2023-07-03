5f4acfa2975ad95e63341b04dcc1965b042a2995
blackevn <kevinlouie55@gmail.com>
2023-07-03 08:06:43 +0000

    Add property

--- types/interfaces.d.ts
+++ types/interfaces.d.ts
@@ -138,18 +138,20 @@ export interface ContextData {
   }
 
   export  type Listing = {
-      createdAt: string;
-      id: string;
-      title: string;
-      description: string;
-      imageSrc: string;
-      category: string;
-      roomCount: number;
-      bathroomCount: number;
-      guestCount: number;
-      locationValue: string;
-      userId: string;
-      price: number;
+
+    id: string
+    title?: string
+    description?: string
+    imageSrc: string
+    createdAt?: string
+    category?:  string
+    roomCount?: number
+    bathroomCount?: number
+    guestCount?: number
+    locationValue: string
+    location?: CountrySelectValue
+    userId?: string
+    price?: number
   }
   
 
