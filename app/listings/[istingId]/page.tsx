1395dc6aa7315b8abee1c124d1e785d160bceeef
blackevn <kevinlouie55@gmail.com>
2023-07-05 08:40:52 +0000

    Add property

--- app/listings/[istingId]/page.tsx
+++ app/listings/[istingId]/page.tsx
@@ -1,39 +1,22 @@
 "use client"
 
 import { getCurrentUser, getListingById, getListings, getReservations } from "@/app/actions";
-import { NextPage } from "next";
 import ListingClient from "./ListingClient";
 import { usePathname } from "next/navigation";
 import useListing from "@/app/hooks/useListing";
 import { useEffect, useState } from "react";
-import axios from "axios";
-
 interface IParams {
   params?: string
 }
 
 const ListingPage = () => {
-   
-   const [ allListings, setAllListings] = useState([])
+
     const pathname = usePathname();
     const listingId = pathname?.toString().replace(/^\/listings\//, "");
+  
+  const { data: data } = useListing(listingId as string)
+  console.log(data);
 
-  useEffect(() => {
-
- if( listingId ){   fetch(`/listings/${listingId}`)
-    .then(res => res.json())
-    .then(data => setAllListings(data))
-    .catch(error => console.log(error)
-    )} return console.log('No ID');
-    
- }, [listingId])
-  
-  // const { data: data } = useListing(listingId as string)
-  // console.log(data);
-  console.log(allListings);
-  
-  
-  
   return (
     <>
     <div>
