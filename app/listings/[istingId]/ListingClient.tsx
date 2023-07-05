16d126556143839d1b08e1a91208f4350cb78ba8
blackevn <kevinlouie55@gmail.com>
2023-07-05 07:53:57 +0000

    Add property

--- app/listings/[istingId]/ListingClient.tsx
+++ app/listings/[istingId]/ListingClient.tsx
@@ -3,29 +3,10 @@
 import { usePathname } from "next/navigation";
 import { useEffect, useMemo } from "react";
 
-export let params: any[] = [];
-
 const ListingClient = () => {
-  const listingParams = usePathname();
 
-  const clearArr = useMemo(() => {
-    if(params.length > 0 )
-      params.shift()
-  }, [params])
-  params.push(listingParams);
 
-  
-
-  // useEffect(() => {
-
-  //   if (params.length > 0) {
-  //     setTimeout(() => {
-  //       params.length = 0;
-  //     }, 5000);
-  //   }
-  // }, [listingParams]);
-
-  return <div>{JSON.stringify(params)}</div>;
+  return <div>ID</div>;
 };
 
 export default ListingClient;
--- app/listings/[istingId]/page.tsx
+++ app/listings/[istingId]/page.tsx
@@ -2,31 +2,40 @@
 
 import { getCurrentUser, getListingById, getListings, getReservations } from "@/app/actions";
 import { NextPage } from "next";
-import ListingClient, { params } from "./ListingClient";
+import ListingClient from "./ListingClient";
 import { usePathname } from "next/navigation";
 import useListing from "@/app/hooks/useListing";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 interface IParams {
   params?: string
 }
 
 const ListingPage = () => {
+   
+   const [ allListings, setAllListings] = useState([])
+    const pathname = usePathname();
+    const listingId = pathname?.toString().replace(/^\/listings\//, "");
+
+  useEffect(() => {
+    axios.get(`/api/listings/${listingId}`)
+    .then(res => setAllListings(res.data))
+    .catch(error => console.log(error)
+    )
+  }, [listingId])
   
   const listingParams = usePathname();
-
-  const pathname = usePathname();
-  const listingId = pathname?.toString().replace(/^\/listings\//, "");
   const { data: data } = useListing(listingId as string)
   console.log(data);
-  console.log(listingId);
+  console.log(allListings);
   
   
   
   return (
     <>
     <div>
-    {JSON.stringify(params)}
-      <ListingClient />
+         <ListingClient />
     </div>
     </>
   );
